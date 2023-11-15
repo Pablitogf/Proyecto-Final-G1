c358257b919ffa39120a6678c0c2a2a44c4c5175
Pablitogf <pablo.gonzalezf@uqvirtual.edu.co>
2023-11-15 10:41:59 -0500

    aaa

--- src/main/java/org/example/Torre.java
+++ src/main/java/org/example/Torre.java
@@ -19,38 +19,46 @@ public class Torre {
 
     public int getId() {
     return id;
-}
+    }
 
     public void setId(int id) {
     this.id = id;
-}
+    }
 
     public String getUbicacion() {
     return ubicacion;
-}
+    }
 
     public void setUbicacion(String ubicacion) {
     this.ubicacion = ubicacion;
-}
+    }
 
     public String getCorreo() {
     return correo;
-}
+    }
 
     public void setCorreo(String correo) {
     this.correo = correo;
-}
+    }
 
     public int getTelefono() {
     return telefono;
-}
+    }
 
     public void setTelefono(int telefono) {
     this.telefono = telefono;
-}
+    }
 
     public void guardarVuelo(Vuelo vueloSupervicion){
         this.vuelos.add(vueloSupervicion);
         System.out.println("Guardo" + vueloSupervicion.toString());
+    }
+
+    public ArrayList<Vuelo> getVuelos() {
+        return vuelos;
+    }
+
+    public void setVuelos(ArrayList<Vuelo> vuelos) {
+        this.vuelos = vuelos;
     }
 }
